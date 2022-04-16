a839fb2e24c333a8ab0d7419dae9b779e882da99
tomato <1324631884@qq.com>
2022-04-16 21:39:59 +0800

    修改错别字：go中除不尽的情况默认向下取整

--- practice_demo/number_int_float.go
+++ practice_demo/number_int_float.go
@@ -6,6 +6,6 @@ func main() {
 	s := []int{2, 2, 1, 1, 1, 2, 2}
 	length := len(s)
 	fmt.Printf("%T,%v\n", length, length)  // int,7
-	fmt.Printf("%T,%v\n", length/2, length/2)  // int,3  除不尽的情况默认向下取证
+	fmt.Printf("%T,%v\n", length/2, length/2)  // int,3  除不尽的情况默认向下取整
 
 }
